e137edf38da0ec17a78b1cd78b1cc4ec37b0ff2f
Jordi Collell <jordic@gmail.com>
2014-12-01 06:32:20 +0100

    session set order key

--- tshop/shop/views.py
+++ tshop/shop/views.py
@@ -145,6 +145,7 @@ def checkout_confirm(request):
     order = get_object_or_404(Order, uid=uid)
     if order.status == 'pendiente':
         order.new_uid()
+        request.session.set(settings.ORDER_KEY, order.uid)
         c = {}
         c['order'] = order
         c['payment_template'] = "shop/%s_payment_form.html" % order.pay_type
